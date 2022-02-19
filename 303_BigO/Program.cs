edcaec88de9c230331971479378192b5d4b128cb
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-02-19 15:07:57 -0800

    303 - Big O notation

---
+++ 303_BigO/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace _303_BigO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* var testInput = new int[] { 10 }; */
+            /* var testInput = Enumerable.Range(0, 1000).ToArray(); */
+            var testInput = Enumerable.Range(0, 10_000).ToArray();
+            /* var testInput = Enumerable.Range(0, 100_000).ToArray(); */
+
+            {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                GetSafe(testInput, 0);
+                stopWatch.Stop();
+                Console.WriteLine($"Finished GetRandom: {stopWatch.Elapsed}");
+            }
+
+            {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                SumArray(testInput);
+                stopWatch.Stop();
+                Console.WriteLine($"Finished SumArray: {stopWatch.Elapsed}");
+            }
+
+            {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                SumNested(testInput);
+                stopWatch.Stop();
+                Console.WriteLine($"Finished SumNested: {stopWatch.Elapsed}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int GetSafe(int[] arr, int index)
+        {
+            if (index >= 0 && index < arr.Length)
+            {
+                return arr[index];
+            }
+            return -1;
+        }
+
+        static int SumArray(int[] arr)
+        {
+            var result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+            return result;
+        }
+
+        static int SumNested(int[] arr)
+        {
+            var result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += i;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    result += j;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 303_BigO_bk/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+int GetRandom(int[] arr)
+{
+    var rnd = new System.Random();
+    return arr[rnd.Next(0, arr.Length)];
+}
+
+int SumArray(int[] arr)
+{
+    var result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += arr[i];
+    }
+    return result;
+}
+
+int SumNested(int[] arr)
+{
+    var result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += i;
+        for (int j = 0; j < arr.Length; j++)
+        {
+            result += j;
+        }
+    }
+
+    return result;
+}
+
+var testInput = new int[] { 10 };
+
+var stopWatch = new Stopwatch();
+
+stopWatch.Start();
+GetRandom(testInput);
+stopWatch.Stop();
+Console.WriteLine($"Finished GetRandom: {stopWatch.Elapsed}");
+stopWatch.Reset();
+
+stopWatch.Start();
+SumArray(testInput);
+stopWatch.Stop();
+Console.WriteLine($"Finished SumArray: {stopWatch.Elapsed}");
+stopWatch.Reset();
+
+stopWatch.Start();
+SumNested(testInput);
+stopWatch.Stop();
+Console.WriteLine($"Finished SumNested: {stopWatch.Elapsed}");
+stopWatch.Reset();
