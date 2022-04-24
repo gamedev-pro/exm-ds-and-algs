37fcad9dae3e716fca803382fbcffe8251e68dcf
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-04-24 15:19:30 -0700

    Revert "315 - Solucao: Arrays e Strings"
    This reverts commit 296d77e1438ddc5b0cc837d7cb5fd77ada802fd8.

--- CodingChallenges/Arrays/Easy/Validate Sequence/ValidateSubsequence.cs
+++ CodingChallenges/Arrays/Easy/Validate Sequence/ValidateSubsequence.cs
@@ -2,6 +2,18 @@ public static class ValidateSubsequenceChallenge
 {
     public static bool ValidateSubSequence(int[] arr, int[] sequence)
     {
-        return false;
+        var currSeqIndex = 0;
+        foreach (var val in arr)
+        {
+            if (sequence[currSeqIndex] == val)
+            {
+                currSeqIndex++;
+            }
+            if (currSeqIndex == sequence.Length)
+            {
+                break;
+            }
+        }
+        return currSeqIndex == sequence.Length;
     }
 }
