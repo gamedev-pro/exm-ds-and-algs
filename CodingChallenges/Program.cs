28453453186e4b9b64b82e3fb76f68fcbe5561f2
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-04-24 15:07:15 -0700

    Remov eindexes from challenge folders

---
+++ CodingChallenges/Program.cs
@@ -0,0 +1,18 @@
+﻿using AlgTester.API;
+
+{
+    var s = TwoSumChallenge.TwoSum;
+    SolutionTester.New().WithSolution(s).Run();
+}
+{
+    var s = ValidateSubsequenceChallenge.ValidateSubSequence;
+    SolutionTester.New().WithSolution(s).Run();
+}
+{
+    var s = PalindromeChallenge.Palindrome;
+    SolutionTester.New().WithSolution(s).Run();
+}
+{
+    var s = FirstNonRepeatingCharacterChallenge.FirstNonRepeatingCharacter;
+    SolutionTester.New().WithSolution(s).Run();
+}
