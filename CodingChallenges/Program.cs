8e73fb4512ecdfd3c3aed277969e08ded1c52e64
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-04-24 15:12:47 -0700

    315 - Solução: Arrays e Strings

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,18 +1,10 @@
 ﻿using AlgTester.API;
 
 {
-    var s = TwoSumChallenge.TwoSum;
-    SolutionTester.New().WithSolution(s).Run();
-}
-{
     var s = ValidateSubsequenceChallenge.ValidateSubSequence;
     SolutionTester.New().WithSolution(s).Run();
 }
 {
     var s = PalindromeChallenge.Palindrome;
-    SolutionTester.New().WithSolution(s).Run();
-}
-{
-    var s = FirstNonRepeatingCharacterChallenge.FirstNonRepeatingCharacter;
     SolutionTester.New().WithSolution(s).Run();
 }
