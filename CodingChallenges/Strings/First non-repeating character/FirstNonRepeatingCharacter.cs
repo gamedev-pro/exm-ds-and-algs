573307a729f23665ddbec4a4fc6847e59538d097
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-04-30 11:28:20 -0700

    Add optimal solution for non repeating character

--- CodingChallenges/Strings/First non-repeating character/FirstNonRepeatingCharacter.cs
+++ CodingChallenges/Strings/First non-repeating character/FirstNonRepeatingCharacter.cs
@@ -1,6 +1,7 @@
 public static class FirstNonRepeatingCharacterChallenge
 {
-    public static int FirstNonRepeatingCharacter(string str)
+    // T: O(n^2), S: O(1)
+    public static int FirstNonRepeatingCharacter_Naive(string str)
     {
         for (int i = 0; i < str.Length; i++)
         {
@@ -13,7 +14,34 @@ public static class FirstNonRepeatingCharacterChallenge
                 }
             }
             if (!foundDuplicate)
-            {	
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // T O(n), S O(1)
+    public static int FirstNonRepeatingCharacter(string str)
+    {
+        var charFreq = new Dictionary<char, int>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            var c = str[i];
+            if (charFreq.TryGetValue(c, out var freq))
+            {
+                charFreq[c] = freq + 1;
+            }
+            else
+            {
+                charFreq[c] = 1;
+            }
+        }
+        for (int i = 0; i < str.Length; i++)
+        {
+            var character = str[i];
+            if (charFreq[character] == 1)
+            {
                 return i;
             }
         }
