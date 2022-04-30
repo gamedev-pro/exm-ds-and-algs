81c9c3d85b4ec8cbf5ebe83e1cc82ab60b2c1edf
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-04-30 11:27:31 -0700

    Revert "323 - Desafio - Hash Tables"
    This reverts commit 1fffb4e5e5223281c0d61978d0b983e7dc475a10.

--- CodingChallenges/Arrays/Easy/TwoSum/TwoSum.cs
+++ CodingChallenges/Arrays/Easy/TwoSum/TwoSum.cs
@@ -1,7 +1,39 @@
 public static class TwoSumChallenge
 {
+    // T: O(n^2), S: O(1)
+    public static int[] TwoSum_Naive(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] + arr[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    // T: O(n), S: O(n)
     public static int[] TwoSum(int[] arr, int target)
     {
+        var valuesToIndexes = new Dictionary<int, int>(arr.Length);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var currentValue = arr[i];
+            var candidate = target - currentValue;
+            if (valuesToIndexes.ContainsKey(candidate))
+            {
+                var j = valuesToIndexes[candidate];
+                return new int[] { j, i };
+            }
+            else
+            {
+                valuesToIndexes.Add(currentValue, i);
+            }
+        }
         return new int[0];
     }
 }
--- CodingChallenges/Strings/First non-repeating character/FirstNonRepeatingCharacter.cs
+++ CodingChallenges/Strings/First non-repeating character/FirstNonRepeatingCharacter.cs
@@ -1,7 +1,50 @@
 public static class FirstNonRepeatingCharacterChallenge
 {
+    // T: O(n^2), S: O(1)
+    public static int FirstNonRepeatingCharacter_Naive(string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            var foundDuplicate = false;
+            for (int j = i + 1; j < str.Length; j++)
+            {
+                if (str[i] == str[j] && i != j)
+                {
+                    foundDuplicate = true;
+                }
+            }
+            if (!foundDuplicate)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // T O(n), S O(1)
     public static int FirstNonRepeatingCharacter(string str)
     {
+        var charFreq = new Dictionary<char, int>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            var c = str[i];
+            if (charFreq.TryGetValue(c, out var freq))
+            {
+                charFreq[c] = freq + 1;
+            }
+            else
+            {
+                charFreq[c] = 1;
+            }
+        }
+        for (int i = 0; i < str.Length; i++)
+        {
+            var character = str[i];
+            if (charFreq[character] == 1)
+            {
+                return i;
+            }
+        }
         return -1;
     }
 }
